166ac470e001b14cf3c5a240d2caaf207c9e0d50
rlrogers <richard@richardlrogers.com>
2019-12-10 18:56:22 -0600

    admin and Recipe app model configs

--- recipe/models.py
+++ recipe/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class RecipeDetails(models.Model):
     # cook = models.ForeignKey("Cook.Model", verbose_name=_("cook"), on_delete=models.CASCADE) Will add Cook App later
-    cook = models.CharField( max_length=50)
-    title = models.CharField(max_length=50)
+    Dish_Author = models.CharField( max_length=50)
+    Dish = models.CharField(max_length=50)
     description = models.CharField(max_length=50)
     number_of_ingredients = models.IntegerField(blank=True)
     ingredient_amount = models.DecimalField( max_digits=2, decimal_places=2)
@@ -15,6 +15,8 @@ class RecipeDetails(models.Model):
     serves_amount = models.IntegerField()
     serves_ppl = models.IntegerField()
     published_date = models.DateTimeField('Recipe made:')
+    class Meta:
+        verbose_name_plural = "RecipeDetails"
     def __str__(self):
         return self.name
     
