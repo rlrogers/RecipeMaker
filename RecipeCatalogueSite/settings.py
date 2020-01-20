e7b30d6d9706ee6373176c74c743bb4257307538
rlrogers <richard@richardlrogers.com>
2020-01-20 16:43:58 -0600

    login/logout pages working and require login for certain routes.

--- RecipeCatalogueSite/settings.py
+++ RecipeCatalogueSite/settings.py
@@ -123,11 +123,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+
 VENV_PATH = os.path.dirname(BASE_DIR)
+
 STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
+
 MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'
+
+LOGIN_URL = 'login'
